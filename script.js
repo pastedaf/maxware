61f4f2e80cee3b840b0609b5d75fe2a33ace5e57
powerracist <topseqpoolfan1@gmail.com>
2025-01-31 16:33:22 +1300

    feat: Add OrbitControls and GLTFLoader to script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
 const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('gridCanvas') });
+const controls = new OrbitControls( camera, renderer.domElement );
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -65,7 +71,7 @@ function updateGridHeights() {
             const dataIndex = Math.floor((i * gridSize + j) / (gridSize * gridSize) * dataArray.length);
             const xOffset = Math.sin(i / gridSize * Math.PI * 2) * 0.5;
             const yOffset = Math.cos(j / gridSize * Math.PI * 2) * 0.5;
-            vertices[index + 2] = (dataArray[dataIndex] / 128 * 3 + xOffset + yOffset);
+            vertices[index + 2] = (dataArray[dataIndex] / 512 * 3 + xOffset + yOffset);
         }
     }
 
